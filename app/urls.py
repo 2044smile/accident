4ed0d7ec64b65f0af9ad82df27ffbc8f16b4bb17
2044smile <2044smile@naver.com>
2021-11-02 15:12:40 +0900

    feat: last commit 13 days ago

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('content_1/', views.content_1, name='content_1'),
+    path('content_2/', views.content_2, name='content_2'),
+    path('content_3/', views.content_3, name='content_3'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'app/index.html')
+
+
+def content_1(request):
+    return render(request, 'app/content_1.html')
+
+
+def content_2(request):
+    return render(request, 'app/content_2.html')
+
+
+def content_3(request):
+    return render(request, 'app/content_3.html')
